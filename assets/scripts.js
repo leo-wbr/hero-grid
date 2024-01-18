c4d642f7229c541e1da3e1129d6dfed5c714eaf4
Leo B <leandrofbermeo@gmail.com>
2024-01-18 17:17:17 +0000

    added gsap library to the page

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,5 @@
+document.documentElement.className = document.documentElement.className.replace(/\bno-js\b/g, '') + 'js';
+
 (function ($) {
   $(window).scroll(function () {
     var navigation = $('.header-bottom');
@@ -14,3 +16,29 @@
     } else navigation.removeClass('on-scroll');
   });
 })(jQuery);
+
+// GSAP
+
+let images = document.querySelectorAll('.front #header_left img');
+let tl = gsap.timeline({
+  immediateRender: false,
+  repeat: 0,
+});
+
+images.forEach(function (el) {
+  tl.to(el, {
+    autoAlpha: 0.7,
+    y: 0,
+    ease: 'power3.inOut',
+  }).to(el, {});
+});
+
+// tl.to(images, {
+//   autoAlpha: 1,
+//   ease: 'elastic.out(1,0.3)',
+//   stagger: {
+//     amount: 1,
+//     from: 'random',
+//     grid: 'auto',
+//   },
+// });
